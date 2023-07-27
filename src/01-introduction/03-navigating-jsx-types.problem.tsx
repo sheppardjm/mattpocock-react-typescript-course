cef73952ac46504db817d2ff1a2c190bf5e0531f
sheppardjm <jsheppard@neucadia.com>
2023-07-26 20:25:51 -0400

    Complete problems 03-05

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,11 +2,14 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      aria-posinset={1}
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={(e) => {}} // React.FormEventHandler<HTMLDivElement> | undefined;
 
       // How do I get autocomplete with JSX?
+      // ctl + space
     />
   );
 };
+
+type Example =  React.FormEventHandler<HTMLDivElement> | undefined;
