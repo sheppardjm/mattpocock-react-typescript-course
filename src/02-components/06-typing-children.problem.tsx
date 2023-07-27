297f5eeb1b13ff9d76396405cd96c235adb43ab0
sheppardjm <jsheppard@neucadia.com>
2023-07-26 21:03:02 -0400

    Complete problem 06 & 07

--- src/02-components/06-typing-children.problem.tsx
+++ src/02-components/06-typing-children.problem.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-export const Button = (props: {}) => {
+type Props = {
+  children?: React.ReactNode;
+}
+
+export const Button = (props: Props) => {
   return <button>{props.children}</button>;
 };
 
 const Parent = () => {
   return (
     <>
-      {/* @ts-expect-error */}
       <Button></Button>
       <Button>Hello world!</Button>
     </>
--- src/02-components/07-typing-onclick-handlers.problem.tsx
+++ src/02-components/07-typing-onclick-handlers.problem.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 interface ButtonProps {
   className: string;
   children: React.ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
 export const Button = ({ children, className, onClick }: ButtonProps) => {
@@ -11,4 +12,8 @@ export const Button = ({ children, className, onClick }: ButtonProps) => {
       {children}
     </button>
   );
+};
+
+const Parent = () => {
+  return <Button className='btn' onClick={(e)=>{}}>Children</Button>;
 };
